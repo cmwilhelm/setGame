76852ab4fcc546af6030562e3e909f9e1ae62623
Chris Wilhelm <chris.wilhelm@gmail.com>
2015-12-06 14:46:30 -0800

    Provide maps between cards and ids and vice versa for easy access

--- library/SetGame/Cards.hs
+++ library/SetGame/Cards.hs
@@ -2,11 +2,13 @@
 {-# LANGUAGE TemplateHaskell    #-}
 module SetGame.Cards where
 
-import SetGame.Attributes
-import Control.Applicative
-import Data.List
-import Data.SafeCopy
-import Data.Aeson (ToJSON(toJSON), object, (.=))
+import           SetGame.Attributes
+import           Control.Applicative
+import           Data.List
+import qualified Data.Map as Map
+import           Data.SafeCopy
+import           Data.Tuple
+import           Data.Aeson (ToJSON(toJSON), object, (.=))
 
 
 data Card = Card {
@@ -20,6 +22,11 @@ instance Show Card where
   show card = "Card: "
             ++ intercalate ", " (fmap (\acc -> acc card) cardAccessors)
 
+instance Ord Card where
+  card1 > card2         = show card1 > show card2
+  card1 <= card2        = show card1 <= show card2
+  card1 `compare` card2 = (show card1) `compare` (show card2)
+
 instance ToJSON Card where
    toJSON (Card color shade number shape) =
      object [ "color"  .= show color
@@ -30,6 +37,17 @@ instance ToJSON Card where
 
 allCards :: [Card]
 allCards = [Card] <*> colors <*> shades <*> numbers <*> shapes
+
+allCardsWithIds :: [(Card, Int)]
+allCardsWithIds = zip allCards [1..]
+
+cardToIdMap :: Map.Map Card Int
+cardToIdMap = Map.fromList allCardsWithIds
+
+idToCardMap :: Map.Map Int Card
+idToCardMap = Map.fromList
+            . fmap swap
+            $ allCardsWithIds
 
 cardAccessors :: [Card -> String]
 cardAccessors = [show . color, show . shade, show . number, show . shape]
