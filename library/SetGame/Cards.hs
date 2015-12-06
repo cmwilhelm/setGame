4883e16fc97507eb995db9fec97c3181f8103485
Chris Wilhelm <chris.wilhelm@gmail.com>
2015-12-06 15:26:49 -0800

    Express the id on the card data structure itself

--- library/SetGame/Cards.hs
+++ library/SetGame/Cards.hs
@@ -15,39 +15,30 @@ data Card = Card {
   color  :: Color,
   shade  :: Shade,
   number :: Number,
-  shape  :: Shape
+  shape  :: Shape,
+  cardId :: Int
   } deriving (Eq)
 
 instance Show Card where
   show card = "Card: "
             ++ intercalate ", " (fmap (\acc -> acc card) cardAccessors)
 
-instance Ord Card where
-  card1 > card2         = show card1 > show card2
-  card1 <= card2        = show card1 <= show card2
-  card1 `compare` card2 = (show card1) `compare` (show card2)
-
 instance ToJSON Card where
-   toJSON (Card color shade number shape) =
+   toJSON (Card color shade number shape cardId) =
      object [ "color"  .= show color
             , "shade"  .= show shade
             , "number" .= show number
-            , "shape"  .= show shape ]
+            , "shape"  .= show shape
+            , "cardId" .= cardId ]
 
 
 allCards :: [Card]
-allCards = [Card] <*> colors <*> shades <*> numbers <*> shapes
-
-allCardsWithIds :: [(Card, Int)]
-allCardsWithIds = zip allCards [1..]
-
-cardToIdMap :: Map.Map Card Int
-cardToIdMap = Map.fromList allCardsWithIds
+allCards = addIds $ [Card] <*> colors <*> shades <*> numbers <*> shapes
+  where addIds partials = zipWith ($) partials [1..]
 
 idToCardMap :: Map.Map Int Card
-idToCardMap = Map.fromList
-            . fmap swap
-            $ allCardsWithIds
+idToCardMap = Map.fromList makeListForCards
+  where makeListForCards = fmap (\card -> (cardId card, card)) allCards
 
 cardAccessors :: [Card -> String]
 cardAccessors = [show . color, show . shade, show . number, show . shape]
--- test/Main.hs
+++ test/Main.hs
@@ -9,13 +9,13 @@ import Test.Hspec
 
 gameState1 :: GameState
 gameState1 = GameState board deck sets
-  where board = [ Card {color=Blue, shade=Solid, number=One, shape=Oval} ]
-        deck  = [ Card {color=Blue,   shade=Solid,   number=One, shape=Squiggle}
-                , Card {color=Green,  shade=Solid,   number=One, shape=Squiggle}
-                , Card {color=Purple, shade=Solid,   number=One, shape=Squiggle}
-                , Card {color=Blue,   shade=Solid,   number=One, shape=Squiggle}
-                , Card {color=Green,  shade=Striped, number=One, shape=Squiggle}
-                , Card {color=Purple, shade=Empty,   number=One, shape=Squiggle} ]
+  where board = [ Card {color=Blue, shade=Solid, number=One, shape=Oval, cardId=1} ]
+        deck  = [ Card {color=Blue,   shade=Solid,   number=One, shape=Squiggle, cardId=1}
+                , Card {color=Green,  shade=Solid,   number=One, shape=Squiggle, cardId=1}
+                , Card {color=Purple, shade=Solid,   number=One, shape=Squiggle, cardId=1}
+                , Card {color=Blue,   shade=Solid,   number=One, shape=Squiggle, cardId=1}
+                , Card {color=Green,  shade=Striped, number=One, shape=Squiggle, cardId=1}
+                , Card {color=Purple, shade=Empty,   number=One, shape=Squiggle, cardId=1} ]
         sets  = []
 
 main :: IO ()
@@ -24,14 +24,14 @@ main = hspec $ do
     it "should move n cards from the top of the deck to the board" $ do
       let (GameState finalBoard finalDeck _) = drawCards 4 gameState1
 
-      let expectedBoard = [ Card {color=Blue,   shade=Solid, number=One, shape=Oval}
-                          , Card {color=Blue,   shade=Solid, number=One, shape=Squiggle}
-                          , Card {color=Green,  shade=Solid, number=One, shape=Squiggle}
-                          , Card {color=Purple, shade=Solid, number=One, shape=Squiggle}
-                          , Card {color=Blue,   shade=Solid, number=One, shape=Squiggle} ]
+      let expectedBoard = [ Card {color=Blue,   shade=Solid, number=One, shape=Oval, cardId=1}
+                          , Card {color=Blue,   shade=Solid, number=One, shape=Squiggle, cardId=1}
+                          , Card {color=Green,  shade=Solid, number=One, shape=Squiggle, cardId=1}
+                          , Card {color=Purple, shade=Solid, number=One, shape=Squiggle, cardId=1}
+                          , Card {color=Blue,   shade=Solid, number=One, shape=Squiggle, cardId=1} ]
 
-      let expectedDeck  = [ Card {color=Green,  shade=Striped, number=One, shape=Squiggle}
-                          , Card {color=Purple, shade=Empty,   number=One, shape=Squiggle} ]
+      let expectedDeck  = [ Card {color=Green,  shade=Striped, number=One, shape=Squiggle, cardId=1}
+                          , Card {color=Purple, shade=Empty,   number=One, shape=Squiggle, cardId=1} ]
 
       finalBoard `shouldBe` expectedBoard
       finalDeck  `shouldBe` expectedDeck
@@ -49,37 +49,37 @@ main = hspec $ do
 
   describe "findSet" $ do
     it "should return the first set it finds" $ do
-      let myBoard     = [ Card {color=Blue,   shade=Solid, number=One,   shape=Oval}
-                        , Card {color=Green,  shade=Solid, number=Two,   shape=Oval}
-                        , Card {color=Green,  shade=Solid, number=One,   shape=Oval}
-                        , Card {color=Purple, shade=Solid, number=Three, shape=Oval} ]
+      let myBoard     = [ Card {color=Blue,   shade=Solid, number=One,   shape=Oval, cardId=1}
+                        , Card {color=Green,  shade=Solid, number=Two,   shape=Oval, cardId=1}
+                        , Card {color=Green,  shade=Solid, number=One,   shape=Oval, cardId=1}
+                        , Card {color=Purple, shade=Solid, number=Three, shape=Oval, cardId=1} ]
 
-          expectedSet = ( Card {color=Blue,   shade=Solid, number=One,   shape=Oval}
-                        , Card {color=Green,  shade=Solid, number=Two,   shape=Oval}
-                        , Card {color=Purple, shade=Solid, number=Three, shape=Oval} )
+          expectedSet = ( Card {color=Blue,   shade=Solid, number=One,   shape=Oval, cardId=1}
+                        , Card {color=Green,  shade=Solid, number=Two,   shape=Oval, cardId=1}
+                        , Card {color=Purple, shade=Solid, number=Three, shape=Oval, cardId=1} )
 
       findSet myBoard `shouldBe` Just expectedSet
 
     it "should return Nothing if there are no sets in the board" $ do
-      let myBoard = [ Card {color=Blue,  shade=Solid, number=One, shape=Oval}
-                    , Card {color=Green, shade=Solid, number=Two, shape=Oval}
-                    , Card {color=Green, shade=Solid, number=One, shape=Oval} ]
+      let myBoard = [ Card {color=Blue,  shade=Solid, number=One, shape=Oval, cardId=1}
+                    , Card {color=Green, shade=Solid, number=Two, shape=Oval, cardId=1}
+                    , Card {color=Green, shade=Solid, number=One, shape=Oval, cardId=1} ]
 
       findSet myBoard `shouldBe` Nothing
 
 
   describe "isSet" $ do
     it "should correctly identify a set as such" $ do
-      let mySet = ( Card {color=Blue,   shade=Solid, number=One,   shape=Oval}
-                  , Card {color=Green,  shade=Solid, number=Two,   shape=Oval}
-                  , Card {color=Purple, shade=Solid, number=Three, shape=Oval} )
+      let mySet = ( Card {color=Blue,   shade=Solid, number=One,   shape=Oval, cardId=1}
+                  , Card {color=Green,  shade=Solid, number=Two,   shape=Oval, cardId=1}
+                  , Card {color=Purple, shade=Solid, number=Three, shape=Oval, cardId=1} )
 
       mySet `shouldSatisfy` isSet
 
     it "should reject a potential set when it fails the qualifications" $ do
-      let mySet = ( Card {color=Blue,   shade=Solid, number=Two,   shape=Oval}
-                  , Card {color=Green,  shade=Solid, number=Two,   shape=Oval}
-                  , Card {color=Purple, shade=Solid, number=Three, shape=Oval} )
+      let mySet = ( Card {color=Blue,   shade=Solid, number=Two,   shape=Oval, cardId=1}
+                  , Card {color=Green,  shade=Solid, number=Two,   shape=Oval, cardId=1}
+                  , Card {color=Purple, shade=Solid, number=Three, shape=Oval, cardId=1} )
 
       mySet `shouldSatisfy` (not . isSet)
 
